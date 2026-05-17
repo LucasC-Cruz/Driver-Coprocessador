f2d3797fbbcd3e1f219269e93fe1bde67b99db5f
Paulo Mota <paulodamota2006@gmail.com>
2026-05-17 16:04:52 -0300

    Adicionado StoreOnMemory
    Funções para guardar na memoria todos os pesos e a imagem enviada

--- src/driver.s
+++ src/driver.s
@@ -21,8 +21,10 @@
 .section .data
 dev_mem: .asciz "/dev/mem"
 
-@ ==================== MAPEAMENTO ====================
 .section .text
+@ ==================== MAPEAMENTO ====================
+@mapea memoria
+@separar em dois (abrir e mapear) para permitir fechar o arquivo de devmem que é aberto!!
 .global mapear
 .type mapear, %function
 mapear:
@@ -44,7 +46,7 @@ mapear:
 
     pop {r4, r5, r7, pc}
 
-
+@fecha endereço mapeado
 .global fechar
 .type fechar, %function
 fechar:
@@ -56,40 +58,46 @@ fechar:
 
 @ ==================== SINAIS DE CONTROLE ==================== 
 
+@inicia a execução da instrução enviada
 .global enable
 .type enable, %function
 enable:
     @r0 deve ser o hps_virtual
-    push {lr}
+    push {r1, lr}
     mov r1, #1
     str r1, [r0, #PIO_ENABLE]
     mov r1, #0
     str r1, [r0, #PIO_ENABLE]
-    pop {pc}
+    pop {r1, pc}
 
+@reseta coprocessador
 .global reset
 .type reset, %function
 reset:
+    push {r1, lr}
     mov r1, #1
     str r1, [r0, #PIO_RESET_COP]
     mov r1, #0
     str r1, [r0, #PIO_RESET_COP]
     bx lr
+    pop {r1, pc}
 
+@limpa operação de erro 
 .global clear_operation
 .type clear_operation, %function
 clear_operation:
-    push {lr}
+    push {r1, lr}
 
     mov r1, #1
     str r1, [r0, #PIO_CLR_OP]
     mov r1, #0
     str r1, [r0, #PIO_CLR_OP]
     
-    pop {pc}
+    pop {r1, pc}
 
 @ ==================== INSTRUÇÕES BÁSICAS ==================== 
 
+@envia o valor de passado em r1 para o pio de instrução 
 .global instruction
 .type instruction, %function
 instruction:
@@ -99,6 +107,7 @@ instruction:
     str r1, [r0, #PIO_INSTRUCTION]
     bx lr
 
+@retorna a instrução que acabou de ser enviada
 .global confirmar
 .type confirmar, %function
 confirmar:
@@ -108,16 +117,19 @@ confirmar:
     ldr r0, [r0, #PIO_CONFIRMAR]
     bx lr
 
+@inicia inferencia
 .global iniciar
 .type iniciar, %function
 iniciar:
+    push {lr}
     @r0 deve ser o hps_virtual
     mov r1, #5
     str r1, [r0, #PIO_INSTRUCTION]
-    @ bl enable
-    @ bl espera_done
-    bx lr
+    bl enable
+    bl espera_done
+    pop {pc}
 
+@não utilizada no coprocessador
 .global status
 .type status, %function
 status:
@@ -126,6 +138,7 @@ status:
     str r1, [r0, #PIO_INSTRUCTION]
     bx lr
 
+@nop
 .global NO_OP
 .type NO_OP, %function
 NO_OP:
@@ -134,6 +147,7 @@ NO_OP:
     str r1, [r0, #PIO_INSTRUCTION]
     bx lr
 
+@retorna resultado da inferencia
 .global get_resultado
 .type get_resultado, %function
 get_resultado:
@@ -142,101 +156,116 @@ get_resultado:
     bx lr
 
 @ ==================== INSTRUÇÔES DE MEMORIA ===================
+@guarda pixel na memoria 
 .global str_img
 .type str_img, %function
 str_img:
     @r0 deve ser o hps_virtual
     @ bl clear_operation              @abaixa a flag de done, colocar no inicio em vez daqui? permitiria conferir a flag no local que fez a chamada  
+    push {r1, lr}       @push r1 para não alterar endereco que sera incrementado depois
     lsl r1, r1, #3      @r1 agr tem o endereço no campo correto 
     lsl r2, r2, #13     @dado lido no campo de dado 
     orr r1, r1, r2      @soma todos os bits em um ergistrador
     str r1, [r0, #PIO_INSTRUCTION]  @guarda instrucao no pio
-    @ bl enable                       @enable na instrucao
-    @ bl espera_done                  @agurda done para retorno, se der erro vai ficar preso pra sempre aqui...
-    bx lr
+    pop {r1, pc}
 
+@guarda bias na memoria 
 .global str_bias
 .type str_bias, %function
 str_bias:
     @r0 deve ser o hps_virtual
     @bl clear_operation
+    push {r1, lr}       @push r1 para não alterar endereco que sera incrementado depois
     lsl r1, r1, #3      @r1 agr tem o endereço no campo correto 
     lsl r2, r2, #10     @dado lido no campo de dado 
     orr r1, r1, r2      @soma todos os bits em um ergistrador
     add r1, r1, #3      @soma op code de store bias
     str r1, [r0, #PIO_INSTRUCTION]  @guarda instrucao no pio
-    @bl enable                       @enable na instrucao
-    @bl espera_done                  @agurda done para retorno
-    bx lr
+    pop {r1, pc}
 
+@guarda beta na memoria 
 .global str_beta
 .type str_beta, %function
 str_beta:
     @r0 deve ser o hps_virtual
+    push {r1,lr}        @push r1 para não alterar endereco que sera incrementado depois
     lsl r1, r1, #3      @r1 agr tem o endereço no campo correto 
     lsl r2, r2, #14     @dado lido no campo de dado 
     orr r1, r1, r2      @soma todos os bits em um ergistrador
     add r1, r1, #4      @soma op code de store beta
     str r1, [r0, #PIO_INSTRUCTION]  @guarda instrucao no pio
-    @ bl enable                       @enable na instrucao
-    @ bl espera_done                  @agurda done para retorno
-    @ bl clear_operation
-    bx lr
+    pop {r1, pc}
 
+@guarda endereco do peso a ser enviado a memoria
+@ endereço passado em r1 !! não há dado para esta funcao!!
 .global str_wadress
 .type str_wadress, %function
 str_wadress:
     @r0 deve ser o hps_virtual
+    push {r1, lr}       @push r1 para não alterar endereco que sera incrementado depois
     lsl r1, r1, #3      @r1 agr tem o endereço no campo correto 
     add r1, r1, #1      @soma op code de store weigth adress
     str r1, [r0, #PIO_INSTRUCTION]  @guarda instrucao no pio
-    @ bl enable                       @enable na instrucao
-    bx lr                           @não tem done, então só volta direto...
+    pop {r1, pc}                           @não tem done, então só volta direto...
 
+@guarda peso na memoria no endereco enviado anteriormente
+@ dado é passado em r2!! não há endereço para esta função!!
 .global str_weight
 .type str_weight, %function
 str_weight:
     @r0 deve ser o hps_virtual
-    lsl r1, r1, #3      @r1 agr tem o dado no campo correto 
-    add r1, r1, #2      @soma op code de store weigth
-    str r1, [r0, #PIO_INSTRUCTION]  @guarda instrucao no pio
-    @ bl enable                       @enable na instrucao
-    @ bl espera_done                  @agurda done para retorno
-    @ bl clear_operation              
-    bx lr
+    push {lr}
+    lsl r2, r2, #3      @r1 agr tem o dado no campo correto 
+    add r2, r2, #2      @soma op code de store weigth
+    str r2, [r0, #PIO_INSTRUCTION]  @guarda instrucao no pio       
+    pop {pc}
 
 @ ==================== FLAGS ==================== 
 
+@espera ocupada para aguarda o sinal de done de um instrução enviada
+@ usar apenas em stores loops, retorna em r2!!!
 .global espera_done
 .type espera_done, %function
 espera_done:
-    push {lr}
-
+    push {r0, r1, lr}
+    mov r2, #0
 espera:
-    @tratamento da flag de erro aqui?
+    ldr r1, [r0, #PIO_FLAG_ERROR]
+    cmp r1, #0                      @compara errro com 0
+    bne erro                        @se houve erro
+
     ldr r1, [r0, #PIO_FLAG_DONE]    @guarda o sinal de done em r1
     cmp r1, #0                      @compara done com 0
-    beq espera                      @se é zero, lê done de novo, até ser 1
+    beq espera
+    b fim                           @se é zero, lê done de novo, até ser 1
+erro:
+    mov r2, #1
+fim:
+    pop {r0, r1, pc}                           @retorna
 
-    pop {pc}                           @retorna
-
+@retorna flag de done
 .global get_flag_done
 .type get_flag_done, %function
 get_flag_done:
     @r0 deve ser o hps_virtual
+    push {lr}
     ldr r0, [r0, #PIO_FLAG_DONE]
-    bx lr
+    pop {pc}
 
+@retorna flag de busy
 .global get_flag_busy
 .type get_flag_busy, %function
 get_flag_busy:
     @r0 deve ser o hps_virtual
+    push {lr}
     ldr r0, [r0, #PIO_FLAG_BUSY]
-    bx lr
+    pop {pc}
 
+@retorna flag de erro
 .global get_flag_error
 .type get_flag_error, %function
 get_flag_error:
     @r0 deve ser o hps_virtual
+    push {lr}
     ldr r0, [r0, #PIO_FLAG_ERROR]
-    bx lr
+    pop {pc}
