e9587e4db7f8d005ad6f2635b6b4183eb2ab526e
Lucas Cruz <102265486+LucasC-Cruz@users.noreply.github.com>
2026-05-18 17:24:51 -0300

    Create api.h

---
+++ src/api.h
@@ -0,0 +1,25 @@
+
+// mapeamento
+extern void* mapear();
+extern void fechar(void* hps_virtual);
+
+//flags
+extern bool get_flag_busy(void* hps_virtual);
+extern bool get_flag_done(void* hps_virtual);
+extern bool get_flag_error(void* hps_virtual);
+extern int  get_resultado(void* hps_virtual);
+
+// ins basicas
+extern void instrucao(void* hps_virtual, int inst);
+extern void iniciar(void* hps_virtual);
+extern void status(void* hps_virtual);
+
+//sinais de controle
+extern void enable(void* hps_virtual);
+extern void reset(void* hps_virtual);
+extern void clear_operation(void* hps_virtual);
+
+extern void store_image(void* hps_virtual);
+extern void store_bias(void* hps_virtual);
+extern void store_beta(void* hps_virtual);
+extern void store_pesos(void* hps_virtual);
