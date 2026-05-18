c64643a635660e688dd3323dc8b2fe41a49a9c40
Lucas Cruz <102265486+LucasC-Cruz@users.noreply.github.com>
2026-05-18 17:23:32 -0300

    atualzação da main
    deixamos bunitin com corzinha

--- src/programa.c
+++ src/programa.c
@@ -2,31 +2,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include "colors.h"
-
-// mapeamento
-extern void* mapear();
-extern void fechar(void* hps_virtual);
-
-//flags
-extern bool get_flag_busy(void* hps_virtual);
-extern bool get_flag_done(void* hps_virtual);
-extern bool get_flag_error(void* hps_virtual);
-extern int  get_resultado(void* hps_virtual);
-
-// ins basicas
-extern void instrucao(void* hps_virtual, int inst);
-extern void iniciar(void* hps_virtual);
-extern void status(void* hps_virtual);
-
-//sinais de controle
-extern void enable(void* hps_virtual);
-extern void reset(void* hps_virtual);
-extern void clear_operation(void* hps_virtual);
-
-extern void store_image(void* hps_virtual);
-extern void store_bias(void* hps_virtual);
-extern void store_beta(void* hps_virtual);
-extern void store_pesos(void* hps_virtual);
+#include "api.h"
 
 
 
@@ -35,53 +11,50 @@ int main() {
     int inst;
     int a;
     int result;
-
-    printf("\n============================\nOlá! Iniciando Coprocessador\n============================");
+    int loop=1;
+    printf("\n============================\nOlá! Iniciando Coprocessador\n============================\n");
     void* hps_virtual = mapear();
 
     if ((intptr_t)hps_virtual == -1) {
         printf("Erro ao mapear memória");
         return 1;
     }
-    printf("Memória mapeada no endereço: %p\n", hps_virtual);
-    printf("\n%s=>%s ", RESET, YELLOW);
+    printf("\nMemória mapeada no endereço: %p\n", hps_virtual);
 
     printf("\n============================\n Iniciando Memória\n============================\n");
-    printf("%sCarregando Bias na memória do Coprocessador%s", YELLOW, RESET);
-    store_bias(hps_virtual);
-    printf("\nTerminei de iniciar bias\n");
-
-    printf("%sCarregando Beta na memória do Coprocessador%s", BLUE, RESET);
+    printf("\nCarregando Bias na memória do Coprocessador...");
+    store_bias(hps_virtual);//nunca vai dar erro pq o codamos bem :)
+    printf("\nCarregando Beta na memória do Coprocessador...");
     store_beta(hps_virtual);
-    printf("\nTerminei de iniciar beta\n");
 
-    printf("%sCarregando Pesos na memória do Coprocessador%s", CYAN, RESET);
+    printf("\nCarregando Pesos na memória do Coprocessador...");
     store_pesos(hps_virtual);
-    printf("\nTerminei de iniciar pesos\n");
-        
 
-    printf("%sPré carregando imagem padrão 4%s",  WHITE, RESET);
+
+    printf("\nPré carregando imagem padrão 4...\n");
     store_image(hps_virtual);
-    printf("\nTerminei de iniciar imagem\n");
+
 
     
-        printf("\nDigite 1 se quiser realizar a inferência da imagem definida\n");
-        printf("\nDigite 2 para trocar a imagem para inferência\n");
-        printf("\nDigite 67 para sair\n");
+        printf("\n%s[1] realizar a inferência da imagem definida%s\n", YELLOW, RESET);
+        printf("\n%s[2] trocar a imagem de inferência%s\n", BLUE, RESET);
+        printf("\n%s[3] para sair%s\n", CYAN, RESET);
         reset(hps_virtual);
         iniciar(hps_virtual);
 
-        printf("Resetando esta buceta\n");
 
-    while( (inst != 67) && (scanf("%d", &inst) == 1)) {
-        printf("\nDigite 1 se quiser realizar a inferência da imagem definida\n");
-        printf("\nDigite 2 para trocar a imagem para inferência\n");
-        printf("\nDigite 67 para sair\n");
+    while(loop==1) {
+        printf("\n%s=>%s ", YELLOW, RESET);
+        scanf("%d", &inst);
+        if(inst == 1){
+            printf("\n========================================================\n");
+            printf("\n[1] realizar a inferência da imagem definida\n");
+            printf("\n[2] trocar a imagem de inferência\n");
+            printf("\n[3] para sair\n");
 
-        if(inst !=2){
             iniciar(hps_virtual);
             result = get_resultado(hps_virtual);
-            printf("\nResultado inferência: %d \n", result);
+            printf("\nResultado inferência:%d\n", result);
 
             done = get_flag_done(hps_virtual); 
             printf("\nFlag de done: %d \n", done);
@@ -92,11 +65,11 @@ int main() {
             error = get_flag_error(hps_virtual);    
             printf("Flag de erro: %d \n", error);
 
-            printf("\n%s=>%s ", YELLOW, RESET);
-        }else {
-            printf("Função não implementada para o Marco 2");
-            inst = 67;
-        }
+            printf("\n========================================================\n");
+
+        }else if (inst ==2){
+            printf("\nFunção não implementada para o Marco 2\n");
+        }else{printf("\nFinalizando o programa\n"); loop=0;}
     }
 
    
